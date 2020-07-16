c4ed0df9b16a772a925e56bb90e789ec428a7cdf
Dorian <sunfury03@gmail.com>
2020-07-16 00:05:02 -0700

    second commit: added prompt integration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,59 @@
 
 const etchASketch = document.querySelector('#etch-a-sketch');
 const containerDiv = document.querySelector('#container-div');
+const clearAndPrompt = document.querySelector('#clearAndPrompt');
 
 let grid = [];
-let dot = document.querySelector('.draw');
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
-    div.classList.add('draw');
+    // div.classList.add('draw');
     grid.push(div);
     
     grid.forEach(function (grid){
         containerDiv.appendChild(div);
     });
+
+    div.addEventListener('mouseover', function (event) {
+        event.target.style.background = "black";
+    });
 }
-function styleBackground (){
-    div.cssText = "background: black;"
-    containerDiv.appendChild(div);
-}
-div.addEventListener('mouseenter', styleBackground);
+
+clearAndPrompt.addEventListener('click', function (){
+
+    containerDiv.innerHTML="";
+
+    
+    let selection = parseInt(prompt("Select the size of the grid", ''));
+
+    if (!selection){
+        return;
+    } else if (isNaN(selection) || selection > 100 || selection < 1){
+        alert("Please enter a number (1-100)");
+        return;
+    } 
+    
+    containerDiv.style.gridTemplateColumns = `repeat(${selection}, 1fr)`;
+
+    for (let j = 0; j < selection^2; j++) {
+        let div = document.createElement('div');
+        grid.push(div);
+        
+        grid.forEach(function (grid){
+            containerDiv.appendChild(div);
+        });
+    
+        div.addEventListener('mouseover', function (event) {
+            event.target.style.background = "black";
+        });
+    }
+});
+
+
+
+    // for (let j = 0; j < 256; j++) {
+    //     grid.pop();
+    // }
 
 
 
